837cccfe1fece91bbc9681926696a182dd3faf87
pepes1234 <103346831+pepes1234@users.noreply.github.com>
2022-08-01 16:20:03 -0300

    Teste de inserção BD

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Projeto_Senai.Model;
+
+Usuario usuario = new Usuario();
+usuario.Nome = "Lucas";
+usuario.Senha = "1234";
+
+ExemploSenaiContext context = new ExemploSenaiContext();
+context.Add(usuario);
+context.SaveChanges();
