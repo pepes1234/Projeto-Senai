90bcc2fc9eaf8d944aff156e645833fbdc7bc25c
pepes1234 <103346831+pepes1234@users.noreply.github.com>
2022-08-01 16:59:20 -0300

    Login e Senha

--- Program.cs
+++ Program.cs
@@ -2,7 +2,58 @@
 
 
 ExemploSenaiContext context = new ExemploSenaiContext();
-foreach (var usuario in context.Usuarios)
+
+bool logincorreto = false;
+bool senhacorreta = false;
+
+while(true)
 {
-    Console.WriteLine(usuario.Nome);
+    Console.Clear();
+    Console.WriteLine("1 - Login, 2 - Senha, 3 - Sair");
+    string num = Console.ReadLine();
+    if(num == "1")
+    {
+        Console.Clear();
+        string login = Console.ReadLine();
+        foreach(var nome in context.Usuarios)
+        {
+            if(login == nome.Nome)
+            {
+                logincorreto = true;
+                if(logincorreto == true && senhacorreta == true)
+                {
+                Console.WriteLine("login efetuado com sucesso!");
+                }
+            }
+        }
+    }
+    else if(num == "2")
+    {
+        Console.Clear();
+        string senha  = Console.ReadLine();
+        foreach(var senha1 in context.Usuarios)
+        {
+            if(senha1.Senha == senha)
+            {
+                senhacorreta = true;
+            }
+            if(logincorreto == true && senhacorreta == true)
+            {
+                Console.WriteLine("login efetuado com sucesso!");
+            }
+        }
+    }
+    else if (num == "3")
+    {
+        break;
+    }
+    else
+    {
+
+    }
+    
+}
+if(logincorreto == true && senhacorreta == true)
+{
+    Console.WriteLine("login efetuado com sucesso!");
 }
